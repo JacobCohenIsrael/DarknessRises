93374d37e686807473e3caf74d9f3b8df2db5d06
jacobbum <jacob@bbumgames.com>
2019-11-17 17:49:03 +0200

    Initial Commit

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Follower : MonoBehaviour
+{
+    private NavMeshAgent myNavMeshAgent;
+    private GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myNavMeshAgent = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myNavMeshAgent.SetDestination(target.transform.position);
+    }
+}

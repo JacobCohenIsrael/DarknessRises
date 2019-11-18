a14c331d722f16de306eab95dd6993f1f9369d5b
jacobbum <jacob@bbumgames.com>
2019-11-18 10:23:47 +0200

    Added Scores and game restart

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,28 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static int score;        // The player's score.
+    public static int score;
 
-
-    Text text;                      // Reference to the Text component.
+    [SerializeField] TextMeshProUGUI text;
 
 
     void Awake()
     {
-        // Set up the reference.
-        text = GetComponent<Text>();
-
-        // Reset the score.
         score = 0;
     }
 
 
     void Update()
     {
-        // Set the displayed text to be the word "Score" followed by the score value.
         text.text = "Score: " + score;
     }
 }
